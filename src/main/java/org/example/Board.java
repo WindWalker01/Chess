8b5685edad5584293355aa08aa72f6f80d863e01
Ruzzel <120955455+WindWalker01@users.noreply.github.com>
2024-08-19 18:10:55 +0800

    Screw Objects hello integers

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -11,49 +11,32 @@ public class Board {
 
     private final int COLUMN = 8;
     private final int ROW = 8;
+    private Texture squareTexture = LoadTexture("src/main/resources/square.png");
 
-    private final Tile[][] tiles = new Tile[COLUMN][ROW];
+    private int[][] square = new int[8][8];
 
     private final Jaylib.Color BLACKTILE = new Jaylib.Color(119, 149, 86, 255);
     private final Jaylib.Color WHITETILE = new Jaylib.Color(235, 236, 208, 255);
 
+    private Texture sample = LoadTexture("src/main/resources/Piece=Bishop, Side=Black.png");
+
     public Board(){
-
-        int c = 0;
-
-        for (int col = 0; col < COLUMN; col++) {
-            for (int row = 0; row < ROW; row++) {
-
-                Tile tile;
-
-                if(c == 0){
-                   tile = new Tile(WHITETILE);
-                   c = 1;
-                }else{
-                    tile = new Tile(BLACKTILE);
-                    c = 0;
-                }
-
-                tile.position = new Jaylib.Vector2(row * 100,  col * 100);
-
-                tiles[col][row] = tile;
-            }
-
-            if(c == 0){
-                c = 1;
-            }else{
-                c = 0;
-            }
-        }
-
+        square[0][0] = 1;
     }
 
     public void drawBoard(){
+
+        //Draw the squares
         for (int col = 0; col < COLUMN; col++) {
             for (int row = 0; row < ROW; row++) {
-                DrawTextureV(Tile.texture, tiles[row][col].position, tiles[col][row].color);
+                DrawTextureV(squareTexture, new Jaylib.Vector2(row * 100, col * 100), (row + col) % 2 == 0 ? WHITETILE : BLACKTILE);
+
+                if(square[col][row] == 1){
+                    DrawTexture(sample, 100 * row, col * 100, RAYWHITE);
+                }
             }
         }
+
     }
 
 }
--- src/main/java/org/example/Pieces/Piece.java
+++ src/main/java/org/example/Pieces/Piece.java
@@ -2,10 +2,19 @@ package org.example.Pieces;
 import static com.raylib.Raylib.*;
 
 
-public abstract class Piece {
+public class Piece {
 
-    public Texture pieceTexture;
-    public Vector2 position = new Vector2().x(0).y(0);
+    public static final int None = 0;
+    public static final int Pawn = 1;
+    public static final int Knight = 2;
+    public static final int Bishop = 3;
+    public static final int Rook = 4;
+    public static final int Queen = 5;
+    public static final int King = 6;
 
-    public abstract void movePiece();
+    public static final int White = 16;
+    public static final int Black = 32;
+
+
+
 }
